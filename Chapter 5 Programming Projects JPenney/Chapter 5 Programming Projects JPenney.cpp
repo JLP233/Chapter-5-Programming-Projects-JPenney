b8c1ff756604ba2e5988d8ee8ea8cfd3366c3160
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-04-09 19:52:39 -0400

    added fstream library and intital input file statement with validation check

--- Chapter 5 Programming Projects JPenney/Chapter 5 Programming Projects JPenney.cpp
+++ Chapter 5 Programming Projects JPenney/Chapter 5 Programming Projects JPenney.cpp
@@ -1,50 +1,23 @@
 // Chapter 5 Programming Projects JPenney.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-// This program calculates population growth per day over time.
+// This program counts the total number of students listed in a text file then displays the alphabetical first and last students' names.
 
 #include <iostream>
-#include <iomanip>
-#include <cmath>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int StartingPopulationSize; 
-	int AverageDailyPopulationIncrease;
-	int ElapsedTimeInDays; 
-	int NewPopulationSize;
+	ifstream inputfile("LineUp.txt")
+		if (!inputfile) {
+			cerr << "Error. Could not open LineUp.txt file" << endl;
+			return 1;
 
+		}
 
-  cout << "Enter Starting Population Size:\n";
-  cin >> StartingPopulationSize;
-
-  cout << "Enter Average Daily Population Increase:\n";
-  cin >> AverageDailyPopulationIncrease;
-
-  cout << "Enter Total Time Elapsed in Days:\n";
-  cin >> ElapsedTimeInDays; 
-
-  if (StartingPopulationSize >= 2 && AverageDailyPopulationIncrease > 0 && ElapsedTimeInDays >= 1)
-  {
-	  
-	  cout << "Population Size After Each Progressive Day:\n";
-
-
-	  for (int Day = 1; Day <= ElapsedTimeInDays; ++Day)
-	  {
-		  NewPopulationSize = StartingPopulationSize + AverageDailyPopulationIncrease * Day;
-		  cout << "Day" << Day << ":" << NewPopulationSize << "\n";
-	  }
-  }
-
-  else
-  {
-	  cout << "Input Error.You must enter a starting population size of 2 or greater.\n";
-	  cout << "You must enter an average daily population increase greater than 0.\n";
-	  cout << "You must enter an elapsed time in days greater than or equal to 1.\n";
-	  return 1;
-  }
+  
 
   return 0;
 }
